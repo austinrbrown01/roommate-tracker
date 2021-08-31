983de776784efd77fa0ae08798a1c9a46d8ce969
Austin <austinrbrown01@gmail.com>
2021-08-30 19:35:20 -0700

    test adding status

--- dynamoHelper.py
+++ dynamoHelper.py
@@ -5,8 +5,18 @@ access_key = aws_creds.aws_access_key_id
 secret_access_key = aws_creds.aws_secret_access_key
 session = boto3.session.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_access_key, region_name="us-west-2")
 
-dynamodb = boto3.resource('dynamodb', region='us-west-2')
+dynamodb = session.client('dynamodb')
 
-table = dynamodb.Table('roommate-status')
 
-print(table.creation_date_time)
+
+def update_roommate_status(name, status):
+    dynamodb.put_item(
+        TableName = 'roommate-status',
+        Item={
+            name: {
+                'S': status
+            }
+        }
+    )
+update_roommate_status('test-austin', "home")
+
