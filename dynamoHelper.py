aa845e5245e5fb4803aa5f9a03486b0cf30404fd
Austin <austinrbrown01@gmail.com>
2021-08-30 19:03:37 -0700

    attempt to get creds working

--- dynamoHelper.py
+++ dynamoHelper.py
@@ -1,6 +1,11 @@
+import secrets
 import boto3
+import aws_creds
+access_key = aws_creds.aws_access_key_id
+secret_access_key = aws_creds.aws_secret_access_key
+session = boto3.session.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_access_key, region_name="us-west-2")
 
-dynamodb = boto3.resouce('dynamodb')
+dynamodb = boto3.resource('dynamodb', region='us-west-2')
 
 table = dynamodb.Table('roommate-status')
 
